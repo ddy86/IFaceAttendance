9f1ed54e154a7b3f766c2de0d1946a191cf7b6f6
daidongyang <henuddy@gmail.com>
2018-09-21 08:34:36 +0800

    add Timer for Thread

--- IFaceAttReader/IFaceAttReader.cs
+++ IFaceAttReader/IFaceAttReader.cs
@@ -16,19 +16,16 @@ namespace IFaceAttReader
 {
     public partial class IFaceAttReader : ServiceBase
     {
-       
-        //the boolean value identifies whether the device is connected
-        private bool bIsConnected = false;
         //the serial number of the device.After connecting the device ,this value will be changed.
         private int iMachineNumber = 1;
 
         public static string IFaceDevices = ConfigurationManager.AppSettings.Get("IFaceDevices");
 
+        int idwErrorCode = 0;
 
         public IFaceAttReader()
         {
             InitializeComponent();
-
 
             // from https://www.codeproject.com/Questions/711973/Using-Zkemkeeper-dll-from-SDK-for-Biometric-scanne
             string[] devices = IFaceDevices.Split(';');
@@ -47,43 +44,37 @@ namespace IFaceAttReader
                     LogHelper.Log(LogLevel.Debug, "device IP & port param set error!");
                     continue;
                 }
-                connectDevice(ip_port[0], int.Parse(ip_port[1]));
+                ConnectDevice(ip_port[0], int.Parse(ip_port[1]));
             }
         }
-        public void connectDevice(string iface_Ip, int port) { 
+
+
+        public void ConnectDevice(string iface_Ip, int port) { 
             Thread createComAndMessagePumpThread = new Thread(() =>
             {
                 LogHelper.Log(LogLevel.Debug, "connectting to device:" + iface_Ip + ":" + port);
                 zkemkeeper.CZKEMClass axCZKEM1 = new zkemkeeper.CZKEMClass();
-                bIsConnected = axCZKEM1.Connect_Net(iface_Ip, port);
+                ReConnect(axCZKEM1, iface_Ip, port);
 
-                int idwErrorCode = 0;
-
-                if (bIsConnected == true)
+                System.Timers.Timer timer = new System.Timers.Timer();
+                timer.Elapsed += new System.Timers.ElapsedEventHandler((object sender, System.Timers.ElapsedEventArgs e) => 
                 {
-                    iMachineNumber = 1;//In fact,when you are using the tcp/ip communication,this parameter will be ignored,that is any integer will all right.Here we use 1.
-                    bool regEvent = axCZKEM1.RegEvent(iMachineNumber, 65535);//Here you can register the realtime events that you want to be triggered(the parameters 65535 means registering all)
-                    LogHelper.Log(LogLevel.Debug, "Connect the device successed: iMachineNumber: " + iMachineNumber);
-                    if (regEvent == true)
+                    string IPAddr = "";
+                    if (axCZKEM1.GetDeviceIP(iMachineNumber, IPAddr))
                     {
-                        LogHelper.Log(LogLevel.Debug, "regEvent value : " + regEvent);
-                        axCZKEM1.OnAttTransactionEx += new zkemkeeper._IZKEMEvents_OnAttTransactionExEventHandler(axCZKEM1_OnAttTransactionEx);
-
+                        LogHelper.Log(LogLevel.Debug, "device " + iface_Ip + ":" + port + " connect status is ok.");
                     }
                     else
                     {
-                        LogHelper.Log(LogLevel.Debug, "regEvent failed, stop app and disconnecting device...");
-                        axCZKEM1.Disconnect();
-                    }
+                        axCZKEM1.GetLastError(ref idwErrorCode);
+                        axCZKEM1.OnAttTransactionEx -= new zkemkeeper._IZKEMEvents_OnAttTransactionExEventHandler(axCZKEM1_OnAttTransactionEx);
+                        LogHelper.Log(LogLevel.Debug, "Unable to connect the device,ErrorCode=" + idwErrorCode.ToString() + ", reConnecting...");
+                        ReConnect(axCZKEM1, iface_Ip, port);
 
-                }
-                else
-                {
-                    axCZKEM1.GetLastError(ref idwErrorCode);
-                    axCZKEM1.OnAttTransactionEx -= new zkemkeeper._IZKEMEvents_OnAttTransactionExEventHandler(axCZKEM1_OnAttTransactionEx);
-                    LogHelper.Log(LogLevel.Debug, "Unable to connect the device,ErrorCode=" + idwErrorCode.ToString(), "Error");
-                    return;
-                }
+                    }
+                });
+                timer.Interval = 600000;// 10 minutes
+                timer.Enabled = true;
 
                 Application.Run();
             });
@@ -92,9 +83,37 @@ namespace IFaceAttReader
             createComAndMessagePumpThread.IsBackground = true;
             createComAndMessagePumpThread.Start();
 
-            LogHelper.Log(LogLevel.Debug ,"Service Started");
+            LogHelper.Log(LogLevel.Debug , iface_Ip + "_" + port + ": Thread Started.");
         }
 
+
+        private void ReConnect(zkemkeeper.CZKEMClass axCZKEM1,string iface_Ip, int port)
+        {
+            bool bIsConnected = axCZKEM1.Connect_Net(iface_Ip, port);
+
+            if (bIsConnected == true){
+                iMachineNumber = 1;//In fact,when you are using the tcp/ip communication,this parameter will be ignored,that is any integer will all right.Here we use 1.
+                bool regEvent = axCZKEM1.RegEvent(iMachineNumber, 65535);//Here you can register the realtime events that you want to be triggered(the parameters 65535 means registering all)
+                LogHelper.Log(LogLevel.Debug, "Connect the device successed. iMachineNumber: " + iMachineNumber);
+                if (regEvent == true)
+                {
+                    LogHelper.Log(LogLevel.Debug, "regEvent value: " + regEvent);
+                    axCZKEM1.OnAttTransactionEx += new zkemkeeper._IZKEMEvents_OnAttTransactionExEventHandler(axCZKEM1_OnAttTransactionEx);
+                }
+                else
+                {
+                    LogHelper.Log(LogLevel.Debug, "regEvent failed, disconnecting device...");
+                    axCZKEM1.Disconnect();
+                }
+            }
+            else
+            {
+                axCZKEM1.GetLastError(ref idwErrorCode);
+                axCZKEM1.OnAttTransactionEx -= new zkemkeeper._IZKEMEvents_OnAttTransactionExEventHandler(axCZKEM1_OnAttTransactionEx);
+                LogHelper.Log(LogLevel.Debug, "Unable to connect the device,ErrorCode=" + idwErrorCode.ToString() + ", reConnect failed.");
+            }
+
+        }
 
 
         private void axCZKEM1_OnAttTransactionEx(string sEnrollNumber, int iIsInValid, int iAttState, int iVerifyMethod, int iYear, int iMonth, int iDay, int iHour, int iMinute, int iSecond, int iWorkCode)
@@ -103,8 +122,6 @@ namespace IFaceAttReader
             string deviceName = Thread.CurrentThread.Name;
             int dataSize = SaveAttData(new IFaceAttendance(sEnrollNumber, iIsInValid, iAttState , iVerifyMethod , iWorkCode ,time, deviceName));
         }
-
-        
 
 
         public int SaveAttData(IFaceAttendance attData)
@@ -116,26 +133,21 @@ namespace IFaceAttReader
             var result = DapperDBContext.Execute(sql, attData); //直接传送list对象
             if (result >= 1)
             {
-                
                 LogHelper.Log(LogLevel.Debug, ": save att :" + JsonConvert.SerializeObject(attData));
-               
-
-
             }
             return result;
             #endregion
         }
 
 
-
         protected override void OnStart(string[] args)
         {
-            LogHelper.Log(LogLevel.Debug, "started. bIsConnected is: " + bIsConnected);
+            LogHelper.Log(LogLevel.Debug, "service started. ");
         }
 
         protected override void OnStop()
         {
-            LogHelper.Log(LogLevel.Debug, "stopping...: " + bIsConnected);
+            LogHelper.Log(LogLevel.Debug, "service stopping...");
             
         }
     }
